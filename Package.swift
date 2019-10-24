1903837cb770590e5230f6b4c1f5e0a5722e90cf
Harish Kumar S <hakumars@in.ibm.com>
2019-10-24 16:43:06 +0530

    change callback signatures

--- Package.swift
+++ Package.swift
@@ -14,17 +14,17 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-nio.git", .branch("master")),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.8.0"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.0.0"),
-        .package(url: "https://github.com/IBM-Swift/Kitura-WebSocket-NIO.git", from: "2.1.1"),
+        .package(url: "https://github.com/harish1992/WebSocketCompression.git", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "WebSocketClient",
-            dependencies: ["NIO", "NIOFoundationCompat", "NIOHTTP1", "NIOSSL", "NIOWebSocket", "NIOConcurrencyHelpers", "NIOExtras", "Kitura-WebSocket"]),
+            dependencies: ["NIO", "NIOFoundationCompat", "NIOHTTP1", "NIOSSL", "NIOWebSocket", "NIOConcurrencyHelpers", "NIOExtras", "WebSocketCompression"]),
         .testTarget(
             name: "WebSocketClientTests",
             dependencies: ["WebSocketClient"]),
